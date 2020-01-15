da284b54ab36f214a4602ccaa845e69c0e9d0384
armon <armon@seas.upenn.edu>
2020-01-15 14:52:49 -0500

    Update performance improvement

--- scan_aggregator/src/scan_aggregator.cpp
+++ scan_aggregator/src/scan_aggregator.cpp
@@ -2,6 +2,8 @@
 // Created by armon on 1/14/20.
 //
 #include <deque>
+#include <tuple>
+#include <iostream>
 
 #include <ros/ros.h>
 #include <message_filters/subscriber.h>
@@ -10,7 +12,6 @@
 #include <nav_msgs/Odometry.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <eigen_conversions/eigen_msg.h>
-
 #include <pcl_ros/point_cloud.h>
 #include <pcl/common/transforms.h>
 
@@ -27,13 +28,13 @@ public:
     {
         float duration;
         float overlap_period;
-        int scanner_frequency;
+        float scanner_frequency;
 
         nh_.param<float>("duration", duration, 0.2);
         nh_.param<float>("overlap_period", overlap_period, 0.0);
-        nh_.param<int>("scanner_frequency", scanner_frequency, 80);
+        nh_.param<float>("scanner_frequency", scanner_frequency, 80.0);
 
-        max_buffer_size_ = round(scanner_frequency * duration);
+        max_buffer_size_ = static_cast<int>(round(scanner_frequency * duration));
         overlap_size_ = std::min(static_cast<int>(round(scanner_frequency * overlap_period)), max_buffer_size_);
 
         sync_.registerCallback(boost::bind(&ScanAggregator::callback, this, _1, _2));
@@ -45,19 +46,22 @@ public:
         Eigen::Isometry3d G;
         tf::poseMsgToEigen(odom_msg->pose.pose, G);
 
-        PointCloud::Ptr world_cloud (new PointCloud());
-        pcl::transformPointCloud(*cloud_msg, *world_cloud, G.cast<float>());
-        cloud_buffer_.push_back(world_cloud);
+        PointCloud::Ptr vehicle_cloud(new PointCloud());
+        *vehicle_cloud = *cloud_msg;
+        cloud_buffer_.emplace_back(vehicle_cloud, G.cast<float>());
 
         if (cloud_buffer_.size() < max_buffer_size_)
             return;
 
-        PointCloud world_aggregate_cloud;
-        for (auto &c : cloud_buffer_)
-            world_aggregate_cloud += *c;
-
+        auto G_inv = G.cast<float>().inverse();
         PointCloud vehicle_aggregate_cloud;
-        pcl::transformPointCloud(world_aggregate_cloud, vehicle_aggregate_cloud, G.cast<float>().inverse());
+        for (auto &e : cloud_buffer_) {
+            PointCloud c;
+            auto ci = std::get<0>(e);
+            auto G0 = std::get<1>(e);
+            pcl::transformPointCloud(*ci, c, G_inv * G0);
+            vehicle_aggregate_cloud += c;
+        }
 
         sensor_msgs::PointCloud2 cloud_out;
         pcl::toROSMsg(vehicle_aggregate_cloud, cloud_out);
@@ -81,7 +85,7 @@ private:
     message_filters::Synchronizer<Policy> sync_;
     ros::Publisher pub_;
 
-    std::deque<PointCloud::Ptr> cloud_buffer_;
+    std::deque<std::tuple<PointCloud::Ptr, Eigen::Isometry3f>> cloud_buffer_;
 
 };
 
